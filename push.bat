acaf7e58b6bf6484602f7ebd89a2b827eb0e3848
CIT142F-23@m307.uc-bcf.edu.ph <CIT142F-23@m307.uc-bcf.edu.ph>
2022-09-14 16:35:30 +0800

    Batch Upload

--- push.bat
+++ push.bat
@@ -1,3 +1,6 @@
-cum
+@echo off
 git remote set-url origin https://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@github.com/albinagain/cit14.git
-cu once more
+git add .
+git commit -m "Batch Upload"
+git push
+pause
