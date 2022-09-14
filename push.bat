6531e107cd26dc290919a02a170b8d234daa6deb
cit142f-23@M307.UC-BCF.EDU.PH <cit142f-23@M307.UC-BCF.EDU.PH>
2022-09-14 16:38:56 +0800

    Batch Upload

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
-git remote set-url origin https://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@github.com/albinagain/cit14.git
+git remote set-url origin https://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@github.com/albinagain/cit14.git
 git add .
 git commit -m "Batch Upload"
 git push
